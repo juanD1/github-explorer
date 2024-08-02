fbe40c01b276c62c5d1e5f37ed57767b20bdc60e
Juan David Coronado Saldaña <juandacor@outlook.com>
2024-08-02 16:47:52 -0500

    adding UI for home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,81 +1,88 @@
-const Home = () => {
+import { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+const Home: NextPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+    <main>
+      <h1 className="text-4xl font-bold mb-8">Welcome to GitHub Search App</h1>
+      <div className="grid gap-8 md:grid-cols-1 lg:grid-cols-2">
+        <div className="p-4 border rounded-md shadow-md">
+          <Image
+            src="/images/repository.svg"
+            alt="repositories"
+            width={64}
+            height={64}
+            className="mx-auto mb-4"
+            priority
+          />
+          <p className="text-base mb-4">
+            Discover GitHub repositories to get inspired and learn about
+            programming. Explore various languages, frameworks, and projects.
+            Find valuable resources to enhance your skills and knowledge.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <Link
+            href="/repositories"
+            className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700"
+          >
+            Search Repositories
+          </Link>
+        </div>
+        <div className="p-4 border rounded-md shadow-md">
+          <Image
+            src="/images/developer.svg"
+            alt="Developers"
+            width={64}
+            height={64}
+            priority
+            className="mx-auto mb-4"
+          />
+          <p className="text-base mb-4">
+            Find software developers' profiles, connect with them, and follow
+            trends in the community. Engage with like-minded professionals,
+            collaborate on projects, and stay updated with the latest
+            advancements in technology.
           </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+          <Link
+            href="/users"
+            className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700"
+          >
+            Search Users
+          </Link>
+        </div>
+        <div className="p-4 border rounded-md shadow-md">
+          <Image
+            src="/images/tech.svg"
+            alt="Technologies"
+            width={64}
+            height={64}
+            priority
+            className="mx-auto mb-4"
+          />
+          <p className="text-base mb-4">
+            Discover repositories showcasing cutting-edge technologies,
+            innovative solutions, and best coding practices. Learn from seasoned
+            developers' code, contribute to open-source projects, and make your
+            mark in the programming world.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        </div>
+        <div className="p-4 border rounded-md shadow-md">
+          <Image
+            src="/images/search.svg"
+            alt="Search"
+            width={64}
+            height={64}
+            priority
+            className="mx-auto mb-4"
+          />
+          <p className="text-base mb-4">
+            Easily find repositories and developer profiles that match your
+            interests with our intuitive search functionality. Start your
+            journey today and unlock the full potential of GitHub's vast
+            repository of knowledge and talent.
           </p>
-        </a>
+        </div>
       </div>
     </main>
   );
